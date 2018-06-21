cea2c75ea4bfb8e8644d39e4c0e16dd0b707340f
dennismuturia <dmuturia2922@gmail.com>
2018-06-21 14:52:44 +0300

    CReated the constants java file. What this is supposed to do is store all the unchanged variables to be used all over the project

---
+++ app/src/main/java/com/muturia/dennis/newmovie_x/Constants.java
@@ -0,0 +1,9 @@
+package com.muturia.dennis.newmovie_x;
+
+public class Constants {
+    /*
+    * In this file we save all the unchanged variables such as the url for the api.
+    * Since there is no api key or api auth, we will just saave the url as it is.
+     */
+    public static final String urlApi = "http://movies.safiriticketing.com/api/movie_events";
+}
