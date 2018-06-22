62ea36e6f7ffada15db18e31105df61fc8e52261
dennismuturia <dmuturia2922@gmail.com>
2018-06-22 15:08:43 +0300

    Calling the getmymovies() method at the main activity to get the response of the api data. Although, when viewing the json data use the .string() method. Not toString().

--- app/src/main/java/com/muturia/dennis/newmovie_x/MainActivity.java
+++ app/src/main/java/com/muturia/dennis/newmovie_x/MainActivity.java
@@ -1,13 +1,54 @@
 package com.muturia.dennis.newmovie_x;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class MainActivity extends Activity {
+    public static final String TAG = MainActivity.class.getSimpleName();//Will be using this to log the data in the logcat
+    @BindView(R.id.hello)TextView apiData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        getMovies();
+    }
+
+    //This method links to the Services class to get the API data
+    public void getMovies(){
+        final Services services = new Services();
+        services.getMyMovies(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()){
+                    final String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            apiData.setText(jsonData);
+                        }
+                    });
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/muturia/dennis/newmovie_x/Services.java
+++ app/src/main/java/com/muturia/dennis/newmovie_x/Services.java
@@ -3,7 +3,6 @@ package com.muturia.dennis.newmovie_x;
 import android.util.Log;
 
 import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -13,7 +12,7 @@ public class Services {
     *In this file we will be calling all the data from the api key
     * Ill start by building the url to call the API
      */
-    public static void getMyMovies(Callback callback){
+    public static void getMyMovies(okhttp3.Callback callback){
         OkHttpClient client = new OkHttpClient.Builder().build();
 
         HttpUrl.Builder builder = HttpUrl.parse(Constants.urlApi).newBuilder();
@@ -21,7 +20,9 @@ public class Services {
 
         Log.d("Url", url);
 
-        Request request = new Request.Builder().url(url).build();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
 
         Call call = client.newCall(request);
         call.enqueue(callback);
